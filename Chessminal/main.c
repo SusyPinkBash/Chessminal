4f81dacc2c5b4cfaba39213d4bd399cd4945d146
Susanna Ardigo <ardigs@usi.ch>
2018-03-22 15:43:16 +0100

    Initial Commit

---
+++ Chessminal/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Chessminal
+//
+//  Created by Susanna Ardigo' on 22.03.18.
+//  Copyright © 2018 Susanna Ardigo'. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
